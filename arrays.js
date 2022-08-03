997685ee2cc19aaece4e32984b1e7bae600249e5
Eleanor Ruth <ellie.ruth@me.com>
2022-08-03 09:42:48 -0600

    Add three array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,18 @@
 var students = ["Sam", "Sydney", "Tamika", "Tom", "Gabi"];
 var testScores = [80, 85, 90, 92, 99];
 var present = [true, true, true, true, false]
+console.log(students);
+console.log(testScores);
+console.log(present);
+
+// expected output: ["Sam", "Sydney", "Tamika", "Tom"]
+students.pop();
+console.log(students);
+
+// expected output: [80, 85, 90, 92, 99, 100]
+testScores.push(100);
+console.log(testScores);
+
+//expected output: [true, true, true, false]
+present.shift();
+console.log(present);
