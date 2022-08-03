0a5b9940fddfadd89db95abbee6763b99b20f966
Eleanor Ruth <ellie.ruth@me.com>
2022-08-03 09:52:31 -0600

    Add index position info

--- arrays.js
+++ arrays.js
@@ -15,4 +15,13 @@ console.log(testScores);
 
 //expected output: [true, true, true, false]
 present.shift();
-console.log(present);
+console.log(present
+
+//demonstrating index position.
+// The index postion is the element number in an array. Counting starts at 0.
+// expected output: "Sam 80 true" Sam's index position in the array is 0.
+
+console.log(students[0], testScores[0], present[0])
+
+//expected output: "Sydney 85 true"
+console.log(students[1], testScores[1], present[1])
